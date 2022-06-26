5bfd7d8416459e4027ad5eb403b7986c2d222289
Nelson <iamnelsonmichael@gmail.com>
2022-06-26 12:29:01 +0100

    :sparkles: add component animation

--- src/views/PinScreen.jsx
+++ src/views/PinScreen.jsx
@@ -6,6 +6,7 @@ import { useOutletContext, useNavigate } from "react-router-dom";
 
 //components
 import { PinInput, PinInputField, Button } from "@chakra-ui/react";
+import AnimatedComponent from "../components/AnimatedComponent";
 
 const PinScreen = ({ back }) => {
   const navigate = useNavigate();
@@ -22,51 +23,53 @@ const PinScreen = ({ back }) => {
     processSettings(pin);
   };
   return (
-    <div className="pin-screen">
-      <div className="wrapper">
-        <p
-          className="done"
-          onClick={() => {
-            navigate(back);
-          }}
-        >
-          Done
-        </p>
-        <main>
-          <div className="pin-inputs">
-            <p>Enter your 4 digit transaction pin</p>
-            <div className="inputs">
-              <PinInput
-                mask
-                size="lg"
-                value={pin}
-                onChange={handleChange}
-                // onComplete={handleComplete}
-                className="pin-input-container"
-              >
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-              </PinInput>
-            </div>
+    <AnimatedComponent>
+      <div className="pin-screen">
+        <div className="wrapper">
+          <p
+            className="done"
+            onClick={() => {
+              navigate(back);
+            }}
+          >
+            Done
+          </p>
+          <main>
+            <div className="pin-inputs">
+              <p>Enter your 4 digit transaction pin</p>
+              <div className="inputs">
+                <PinInput
+                  mask
+                  size="lg"
+                  value={pin}
+                  onChange={handleChange}
+                  // onComplete={handleComplete}
+                  className="pin-input-container"
+                >
+                  <PinInputField />
+                  <PinInputField />
+                  <PinInputField />
+                  <PinInputField />
+                </PinInput>
+              </div>
 
-            <div className="submit-button">
-              <Button
-                size="md"
-                colorScheme="red"
-                onClick={() => {
-                  handleComplete(pin);
-                }}
-                isLoading={states.loading}
-              >
-                Save Information
-              </Button>
+              <div className="submit-button">
+                <Button
+                  size="md"
+                  colorScheme="red"
+                  onClick={() => {
+                    handleComplete(pin);
+                  }}
+                  isLoading={states.loading}
+                >
+                  Save Information
+                </Button>
+              </div>
             </div>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
-    </div>
+    </AnimatedComponent>
   );
 };
 
