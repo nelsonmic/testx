463bd15f9c79a17e8f9b8e6f083b0df4bbf725a5
nelsonmichael <iamnelsonmichael@gmail.com>
2022-07-29 12:10:12 +0100

    complete balance reveal

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 export { truncateText } from "./truncateText";
+export { truncateTextWithAsterix } from "./truncateTextWithAsterix";
 export { truncateDecimals } from "./truncateDecimals";
 export { numbersWithCommas } from "./numbersWithCommas";
 export { validateEmail } from "./validate";
---
+++ src/views/overview/Royalties.jsx
@@ -0,0 +1,22 @@
+import AnimatedPage from "../../components/AnimatedPage";
+import BackButton from "../../components/BackButton";
+
+const Royalties = () => {
+
+
+      return (
+            <div className="transactions-summary">
+                  <BackButton />
+                  <h1 className="page-name">Royalties</h1>
+                  <AnimatedPage>
+                        <div className="wrapper">
+                              <main>
+
+                              </main>
+                        </div>
+                  </AnimatedPage>
+            </div>
+      );
+};
+
+export default Royalties;
